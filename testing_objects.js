4d5f4e96e8376c794e49642c30e55b977e489448
Macilo <1124513@students.wits.ac.za>
2020-03-08 20:59:26 +0200

    testing objects

---
+++ testing_objects.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const student = {
+  name: 'Kwezi',
+  studentNumber: 453528
+}
+console.log(student) // print the entire object
+// access the object's properties
+console.log(`${student.name}'s student number is ${student.studentNumber}`)
+
+// this function manipualtes the object
+const addAge = function (theStudent, age) {
+  theStudent.age = age // add a new property called age to the student
+}
+addAge(student, 20)
+
+console.log(`${student.age} is the age of ${student.name}`)
