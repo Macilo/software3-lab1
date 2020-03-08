ad2ba3b9263b41cb2c21d717e299cbb4ad19012e
Macilo <1124513@students.wits.ac.za>
2020-03-08 20:59:49 +0200

    testing strings

---
+++ testing_strings.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const a = 5
+const b = 10
+
+// using ordinary string
+console.log('Fifteen is ' + (a + b) + ' and not ' + (2 * a + b) + '.')
+
+// using template strings
+console.log(`Fifteen is ${a + b} and not ${2 * a + b}.`)
